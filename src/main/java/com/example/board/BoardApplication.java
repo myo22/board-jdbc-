9d52cc5c38a8c594cd9d79dd488423abd7f759b4
myo22 <dlalsgud12@naver.com>
2023-05-11 18:04:49 +0900

    게시판

---
+++ src/main/java/com/example/board/BoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApplication k{
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardApplication.class, args);
+    }
+
+}
