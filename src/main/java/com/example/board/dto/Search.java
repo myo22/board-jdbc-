216d77d294326020a19b5f3c8764554f724f4693
myo22 <dlalsgud12@naver.com>
2023-06-04 18:24:15 +0900

    검색

---
+++ src/main/java/com/example/board/dto/Search.java
@@ -0,0 +1,55 @@
+package com.example.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Search {
+
+    private String type;
+    private String keyword;
+    public String[] getTypes(){
+        if(type == null || type.isEmpty()){
+            return null;
+        }
+        return type.split("");
+    }
+
+    public Pageable getPageable(String...props) {
+        return PageRequest.of(this.page -1, this.size, Sort.by(props).descending());
+    }
+
+    private String link;
+
+    public String getLink() {
+
+        if(link == null){
+            StringBuilder builder = new StringBuilder();
+
+            builder.append("page=" + this.page);
+
+            builder.append("&size=" + this.size);
+
+
+            if(type != null && type.length() > 0){
+                builder.append("&type=" + type);
+            }
+
+            if(keyword != null){
+                try {
+                    builder.append("&keyword=" + URLEncoder.encode(keyword,"UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                }
+            }
+            link = builder.toString();
+        }
+
+        return link;
+    }
+
+}
